0007675cbb39d7110dffd8bf5eb3f8aedb01a706
Darius Kazemi <darius.kazemi@gmail.com>
2018-03-28 12:10:49 -0700

    first

---
+++ README.md
@@ -0,0 +1,7 @@
+# weird-brackets (Weird Brackets)
+
+Generating bracket memes
+
+## License
+Copyright (c) 2018 Darius Kazemi  
+Licensed under the MIT license.
---
+++ config.js
@@ -0,0 +1,7 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+  consumer_key:         'a',
+  consumer_secret:      'a',
+  access_token:         'a',
+  access_token_secret:  'a'
+};
